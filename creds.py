78592d3076fe200543ae0cf4d3911f87f0b39fe9
Yahir Tapia <105319923+Luughx@users.noreply.github.com>
2023-09-14 21:21:38 -0600

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,5 +1,4 @@
-DISCORD_TOKEN = "MTA5NDA4NjgwNTM5NTA5MTU4Nw.GWWw08.Hd3vYgwkBql3hnmovIcRo-kLBVNkbpgLHqopHE" #MTA5NTE2Nzg2OTc2NTAyOTg5MA.Grnyzp.sX_zABYate7IpocpXllh6OyNRgRgTt6JKLOdrY
-OPENAI_API_KEY = "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"
-MONGODB_URI = "mongodb+srv://production:DebQCkw1DTN1mRiy@lumi-openaiv2.wrmpsdz.mongodb.net/test" #mongodb+srv://developer:dUODrTYQMilEav1x@lumi-openaiv2.wrmpsdz.mongodb.net/test
-
+DISCORD_TOKEN = ""
+OPENAI_API_KEY = ""
+MONGODB_URI = "" 
 
